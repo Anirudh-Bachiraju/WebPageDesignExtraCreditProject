4723d4171d45a6c0762aa0aa23295c1caeb0d8e3
Anirudh-Bachiraju <32845022+Anirudh-Bachiraju@users.noreply.github.com>
2017-12-29 18:23:21 -0500

    Update1

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 <html>
 <body>
 <?php
-$myfile = fopen("data.txt", "r") or die("Unable to open file!");
+$myfile = fopen("data.txt", "a+") or die("Unable to open file!");
 // Output one line until end-of-file
 while(!feof($myfile)) {
   echo fgets($myfile) . "<br>";
 }
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
 fclose($myfile);
 ?>
 </body>
