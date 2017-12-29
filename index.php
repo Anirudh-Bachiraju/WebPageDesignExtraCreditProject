c1ae22bdcc22e14ac9e42408d65f062a135ec92d
Anirudh-Bachiraju <32845022+Anirudh-Bachiraju@users.noreply.github.com>
2017-12-29 18:19:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 <?php
-$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+$myfile = fopen("data.txt", "r") or die("Unable to open file!");
 // Output one line until end-of-file
 while(!feof($myfile)) {
   echo fgets($myfile) . "<br>";
