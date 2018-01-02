36ee861bd8b98436b4b1bf71a082bad61af7bd07
Anirudh-Bachiraju <32845022+Anirudh-Bachiraju@users.noreply.github.com>
2018-01-01 23:17:47 -0500

    Update index.php

--- index.php
+++ index.php
@@ -24,16 +24,16 @@
 <?php
 
 	// Open the text file
-	$f = fopen("textfile.txt", "w");
+	$f = fopen("data.txt", "w");
 
 	// Write text
-	fwrite($f, $_POST["textblock"]); 
+	fwrite($f, $_POST["post"]); 
 
 	// Close the text file
 	fclose($f);
 
 	// Open file for reading, and read the line
-	$f = fopen("textfile.txt", "r");
+	$f = fopen("data.txt", "r");
 
 	// Read text
 	echo fgets($f); 
