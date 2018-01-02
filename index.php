cc2d684aa09fbe0a20104435055016771f16d7fc
Anirudh-Bachiraju <32845022+Anirudh-Bachiraju@users.noreply.github.com>
2018-01-01 22:49:35 -0500

    Update index.php

--- index.php
+++ index.php
@@ -22,13 +22,120 @@
 	<br />
 	<hr />
 <?php
-$myfile = fopen("data.txt", "a+") or die("Unable to open file!");
-// Output one line until end-of-file
-$txt = $_POST["post"];
-fwrite($myfile, $txt);
-while(!feof($myfile)) {
-  echo fgets($myfile) . "<br />";
+if(isset($_POST['post'])) {
+    $data = $_POST['post'] . "\n";
+    $ret = file_put_contents('data.txt', $data, FILE_APPEND | LOCK_EX);
+    if($ret === false) {
+        die('There was an error writing this file');
+    }
+    else {
+        echo "$ret bytes written to file";
+    }
 }
+else {
+   die('no post data to process');
+}
+?>
+<?php
+/* creates an html element, like in js */
+class html_element
+{
+	/* vars */
+	var $type;
+	var $attributes;
+	var $self_closers;
+	
+	/* constructor */
+	function html_element($type,$self_closers = array('input','img','hr','br','meta','link'))
+	{
+		$this->type = strtolower($type);
+		$this->self_closers = $self_closers;
+	}
+	
+	/* get */
+	function get($attribute)
+	{
+		return $this->attributes[$attribute];
+	}
+	
+	/* set -- array or key,value */
+	function set($attribute,$value = '')
+	{
+		if(!is_array($attribute))
+		{
+			$this->attributes[$attribute] = $value;
+		}
+		else
+		{
+			$this->attributes = array_merge($this->attributes,$attribute);
+		}
+	}
+	
+	/* remove an attribute */
+	function remove($att)
+	{
+		if(isset($this->attributes[$att]))
+		{
+			unset($this->attributes[$att]);
+		}
+	}
+	
+	/* clear */
+	function clear()
+	{
+		$this->attributes = array();
+	}
+	
+	/* inject */
+	function inject($object)
+	{
+		if(@get_class($object) == __class__)
+		{
+			$this->attributes['text'].= $object->build();
+		}
+	}
+	
+	/* build */
+	function build()
+	{
+		//start
+		$build = '<'.$this->type;
+		
+		//add attributes
+		if(count($this->attributes))
+		{
+			foreach($this->attributes as $key=>$value)
+			{
+				if($key != 'text') { $build.= ' '.$key.'="'.$value.'"'; }
+			}
+		}
+		
+		//closing
+		if(!in_array($this->type,$this->self_closers))
+		{
+			$build.= '>'.$this->attributes['text'].'</'.$this->type.'>';
+		}
+		else
+		{
+			$build.= ' />';
+		}
+		
+		//return it
+		return $build;
+	}
+	
+	/* spit it out */
+	function output()
+	{
+		echo $this->build();
+	}
+}
+?>
+<?php
+$my_anchor = new html_element('p');
+$my_anchor->set('text',$data);
+$my_anchor->set('id','1');
+$my_anchor->output();
 ?>
 </body>
 
